49cc875c209580fde00b417386afbdacaf67d21a
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-05-04 16:24:53 -0600

    add thumbnails

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-var request = new XMLHttpRequest();
+var request3 = new XMLHttpRequest();
 
-request.onreadystatechange = function() {
-  if( request.readyState === 4 && request.status === 200 ) {
+request3.onreadystatechange = function() {
+  if( this.readyState === 4 && this.status === 200 ) {
 
-    var jsonObj = JSON.parse(request.responseText);
+    var jsonObj = JSON.parse(this.responseText);
     var array = [];
     var urls = [];
+    var thumbnails = [];
 
     for (var i = 0; i < jsonObj.data.children.length; i++) {
-      array.push(jsonObj.data.children[i].data.title);
-      urls.push('https://reddit.com' + jsonObj.data.children[i].data.permalink);
+      array.push( jsonObj.data.children[i].data.title );
+      urls.push( 'https://reddit.com' + jsonObj.data.children[i].data.permalink );
+      thumbnails.push( jsonObj.data.children[i].data.thumbnail );
     }
     for(var j = 0; j < array.length; j++) {
       $('ul').append('<li>');
-      $('li').eq(j).append('<a href=' + urls[j] + '>');
+      $('li').eq(j).append( '<img src=' + "'" + thumbnails[j] + "'" + '>');
+      $('li').eq(j).append('<a href=' + "'" + urls[j] + "'" + '>');
       $('a').eq(j).text( array[j] );
     }
   }
 };
 
-request.open('GET', 'https://www.reddit.com/r/starwars.json');
-
-request.send();
+request3.open('GET', 'https://www.reddit.com/r/starwars.json');
+request3.send();
